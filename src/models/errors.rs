478191da1c23b5d095889c71b64ccbbff6c2b6b7
Igor Braga <higorb1@gmail.com>
2022-10-31 18:11:49 -0400

    Fix formatting
    Signed-off-by: Igor Braga <higorb1@gmail.com>

--- src/models/errors.rs
+++ src/models/errors.rs
@@ -3,7 +3,6 @@ use thiserror::Error;
 #[derive(Error, Debug)]
 pub enum OrderbookError {
     // TODO: Future version, add Custom Error handling
-
     #[error(transparent)]
     Other(#[from] anyhow::Error),
 }
